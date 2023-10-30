0c8b290fea3770e2c658d16f8fa82d8c8409dcc7
timso-iscte <timso@iscte-iul.pt>
2023-10-30 15:49:05 +0000

    Added GEt requests for movies with most 5 stars and movies with best average score

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -17,6 +17,42 @@ router.post("/", async (req, res) =>{
 	res.send(results).status(200);
 })
 
+// Displays list of top scored movies (by average
+// score), sorted in descending order. Show movie
+// information (title, year, etc.) in the response.
+// Limit to {num_movies}
+
+router.get("/higher/:num_movies", async (req, res) => {
+	var num_movies = parseInt(req.params.num_movies);
+	let results = await db.collection('users').aggregate([
+		
+		{$project: {"movies":1}},
+		{$unwind: "$movies"},
+		{$group: {_id:"$movies.movieid", avg_rating:{$avg:"$movies.rating"}}},
+		{$sort: {avg_rating:-1}},
+		{$limit: num_movies},
+		{$lookup: {from: "movies", localField: "_id", foreignField:"_id", as: "info"}},
+		{$project: {"_id": 0, "info.title": 1, "info.year": 1, "info.genres": 1, "avg_rating": 1}}
+		
+	]).toArray()
+	res.send(results).status(200)
+})
+
+// Return movies with most 5 stars. Show movie
+// information
+router.get("/stars", async (req, res) => {
+	let results = await db.collection('users').aggregate([
+		{$project: {"movies":1}},
+		{$unwind: "$movies"},
+		{$match: {"movies.rating": {$eq: 5}}},
+		{$group: {_id:"$movies.movieid", nmr_of_5stars:{$sum:1}}},
+		{$lookup: {from: "movies", localField: "_id", foreignField:"_id", as: "info"}},
+		{$sort: {nmr_of_5stars: -1}},
+		{$project: {"_id": 0, "info.title": 1, "info.year": 1, "info.genres": 1, "nmr_of_5stars": 1}}
+	]).toArray()
+	res.send(results).status(200)
+})
+
 //retrieves movie by _id
 router.get("/:id", async (req, res) => {
 	var id = new ObjectId(req.params.id);
@@ -48,26 +84,7 @@ router.put("/:id", async (req, res) => {
 	res.send(results).status(200)
 })
 
-// Displays list of top scored movies (by average
-// score), sorted in descending order. Show movie
-// information (title, year, etc.) in the response.
-// Limit to {num_movies}
 
-router.get("/higher/:num_movies", async (req, res) => {
-	var num_movies = req.params.num_movies;
-	let results = db.collection('users').aggregate([
-		{$project: {"movies":1}},
-		{$unwind: "$movies"},
-		{$group: {_id:"$movies.movieid", avg_rating:{$avg:"$movies.rating"}}},
-		{$sort: {avg_rating:-1}},
-		{$limit: num_movies}
-		
-	])
-	res.send(results).status(200)
-
-	
-
-})
 
 
 export default router;
